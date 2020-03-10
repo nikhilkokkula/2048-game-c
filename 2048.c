7bb54e6e17aeee7e55d9c8f8f75cb19c4f07a3a9
nikhilkokkula <nikhilkokkula25@gmail.com>
2020-03-11 00:58:00 +0530

    Initial commit for 2048 using c language

---
+++ 2048.c
@@ -0,0 +1,277 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ncurses.h>
+void pri(int a[][4],int f);
+void main()
+{
+	int a[4][4],i,j,m,n,f=0;
+	char x;
+	for(i=0;i<=3;i++)
+	{
+		for(j=0;j<=3;j++)
+		{
+			a[i][j]=0;
+		}
+	}
+	a[rand()%4][rand()%4]=2;
+	while(1)
+	{
+	    //clrscr();
+		while(1)
+		{
+			m=rand()%4;
+			n=rand()%4;
+			if(a[m][n]==0)
+			{
+				a[m][n]=2;
+				system("cls");
+				pri(a,f);
+				break;
+			}
+		}
+		//scanf("%c",&x);
+		x=getch();
+		if(x=='a')
+		{
+        for(i=0;i<=3;i++)
+		{
+            if(a[i][0]==0)
+			{
+                if(a[i][1]==0)
+				{
+                    if(a[i][2]==0)
+					{
+                        a[i][0]=a[i][3];
+                        a[i][3]=0;
+						}
+                    else
+					{
+                        a[i][0]=a[i][2];
+                        a[i][2]=0;
+						}
+						}
+                else
+				{
+                    a[i][0]=a[i][1];
+                    a[i][1]=0;
+					}
+					}
+            if(a[i][1]==0)
+			{
+                if(a[i][2]==0)
+				{
+                    a[i][1]=a[i][3];
+                    a[i][3]=0;
+					}
+                else
+				{
+                    a[i][1]=a[i][2];
+                    a[i][2]=0;
+					}
+					}
+            if(a[i][2]==0)
+			{
+                a[i][2]=a[i][3];
+                a[i][3]=0;
+				}
+            if(a[i][0]==a[i][1])
+			{
+                a[i][0]+=a[i][1];
+                if(a[i][2]==a[i][3])
+				{
+                    a[i][1]=a[i][2]+a[i][3];
+                    a[i][2]=0;
+                    a[i][3]=0;
+					}
+                else
+				{
+                    a[i][1]=a[i][2];
+                    a[i][2]=a[i][3];
+                    a[i][3]=0;
+					}
+					}
+            else if(a[i][1]==a[i][2])
+			{
+                a[i][1]+=a[i][2];
+                a[i][2]=a[i][3];
+                a[i][3]=0;
+				}
+            else if(a[i][2]==a[i][3])
+			{
+                a[i][2]+=a[i][3];
+                a[i][3]=0;
+				}
+				}
+				}
+		else if(x=='w')
+		{
+        for(i=0;i<=3;i++)
+		{
+            if(a[0][i]==0)
+			{
+                if(a[1][i]==0)
+				{
+                    if(a[2][i]==0){
+                        a[0][i]=a[3][i];
+                        a[3][i]=0;
+						}
+                    else
+					{
+                        a[0][i]=a[2][i];
+                        a[2][i]=0;
+						}
+						}
+                else
+				{
+                    a[0][i]=a[1][i];
+                    a[1][i]=0;
+					}
+					}
+            if(a[1][i]==0){
+                if(a[2][i]==0){
+                    a[1][i]=a[3][i];
+                    a[3][i]=0;}
+                else {
+                    a[1][i]=a[2][i];
+                    a[2][i]=0;}}
+            if(a[2][i]==0){
+                a[2][i]=a[3][i];
+                a[3][i]=0;
+				}
+            if(a[0][i]==a[1][i]){
+                a[0][i]+=a[1][i];
+                if(a[2][i]==a[3][i]){
+                    a[1][i]=a[2][i]+a[3][i];
+                    a[2][i]=0;
+                    a[3][i]=0;}
+                else{
+                    a[1][i]=a[2][i];
+                    a[2][i]=a[3][i];
+                    a[3][i]=0;}
+					}
+            else if(a[1][i]==a[2][i]){
+                a[1][i]+=a[2][i];
+                a[2][i]=a[3][i];
+                a[3][i]=0;}
+            else if(a[2][i]==a[3][i]){
+                a[2][i]+=a[3][i];
+                a[3][i]=0;}
+				}
+				}
+		else if(x=='s')
+		{
+        for(i=0;i<=3;i++)
+		{
+            if(a[3][i]==0){
+                if(a[2][i]==0){
+                    if(a[1][i]==0){
+                        a[3][i]=a[0][i];
+                        a[0][i]=0;}
+                    else{
+                        a[3][i]=a[1][i];
+                        a[1][i]=0;}}
+                else{
+                    a[3][i]=a[2][i];
+                    a[2][i]=0;}}
+            if(a[2][i]==0){
+                if(a[1][i]==0){
+                    a[2][i]=a[0][i];
+                    a[0][i]=0;}
+                else{
+                    a[2][i]=a[1][i];
+                    a[1][i]=0;}}
+            if(a[1][i]==0){
+                a[1][i]=a[0][i];
+                a[0][i]=0;}
+            if(a[3][i]==a[2][i]){
+                a[3][i]+=a[2][i];
+                if(a[1][i]==a[0][i]){
+                    a[2][i]=a[1][i]+a[0][i];
+                    a[1][i]=0;
+                    a[0][i]=0;}
+                else{
+                    a[2][i]=a[1][i];
+                    a[1][i]=a[0][i];
+                    a[0][i]=0;}}
+            else if(a[2][i]==a[1][i]){
+                a[2][i]+=a[1][i];
+                a[1][i]=a[0][i];
+                a[0][i]=0;}
+            else if(a[1][i]==a[0][i]){
+                a[1][i]+=a[0][i];
+                a[0][i]=0;}
+		}
+		}
+		else if(x=='d'){
+        for(i=0;i<=3;i++){
+            if(a[i][3]==0){
+                if(a[i][2]==0){
+                    if(a[i][1]==0){
+                        a[i][3]=a[i][0];
+                        a[i][0]=0;}
+                    else{
+                        a[i][3]=a[i][1];
+                        a[i][1]=0;}}
+                else{
+                    a[i][3]=a[i][2];
+                    a[i][2]=0;}}
+            if(a[i][2]==0){
+                if(a[i][1]==0){
+                    a[i][2]=a[i][0];
+                    a[i][0]=0;}
+                else{
+                    a[i][2]=a[i][1];
+                    a[i][1]=0;}}
+            if(a[i][1]==0){
+                a[i][1]=a[i][0];
+                a[i][0]=0;}
+            if(a[i][3]==a[i][2]){
+                a[i][3]+=a[i][2];
+                if(a[i][1]==a[i][0]){
+                    a[i][2]=a[i][1]+a[i][0];
+                    a[i][1]=0;
+                    a[i][0]=0;}
+                else{
+                    a[i][2]=a[i][1];
+                    a[i][1]=a[i][0];
+                    a[i][0]=0;}
+            }
+            else if(a[i][2]==a[i][1])
+                {
+                a[i][2]+=a[i][1];
+                a[i][1]=a[i][0];
+                a[i][0]=0;}
+            else if(a[i][1]==a[i][0]){
+                a[i][1]+=a[i][0];
+                a[i][0]=0;}
+				}
+				}
+				for(i=0;i<=3;i++)
+                {
+                    for(j=0;j<=3;j++)
+                    {
+                        if(a[i][j]==2048){
+                            f=1;
+                        break;
+                        }
+                    }
+                }
+	}
+}
+void pri(int a[4][4],int f)
+{
+	int i,j;
+	if(f==1)
+        printf("\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\tYOU WIN");
+    else{
+	printf("\n\n\n\n\n\t\t\tUSE 'A' as <-(move left)\n\t\t\t    'S' as (move down)\n\t\t\t    'D' as ->(move right)\n\t\t\t    'W' as (move up)\n\n\n\n\n");
+	for(i=0;i<=3;i++)
+	{
+		for(j=0;j<=3;j++)
+		{
+			printf("\t%d\t",a[i][j]);
+		}
+		printf("\n");
+	}
+    }
+}
